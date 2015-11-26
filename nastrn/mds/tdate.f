8ce5fccb1450d393c305fa9e3b07a1148b46ae96
furyofdragon <furyofdragon@gmail.com>
2015-11-27 00:34:55 +0300

    idate switched from VMS to standard

--- nastrn/mds/tdate.f
+++ nastrn/mds/tdate.f
@@ -8,8 +8,16 @@ C
 C     THIS ROUTINE OBTAINS THE MONTH, DAY AND YEAR, IN INTEGER FORMAT   
 C        
       INTEGER DATE(3)        
+      integer ndate(3)
 C        
-      CALL IDATE (DATE(1),DATE(2),DATE(3))        
+!      CALL IDATE (DATE(1),DATE(2),DATE(3))
 C                 MONTH   DAY     YEAR        
+      call idate(ndate)
+!     day, month, and year
+
+      DATE(1) = ndate(2)
+      DATE(2) = ndate(1)
+      DATE(3) = ndate(3)
+
       RETURN        
       END        
